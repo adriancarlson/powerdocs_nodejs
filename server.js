09ed04213d2188665b3491a885f8859827e55562
Adrian Carlson <adrian.r.carlson@gmail.com>
2022-09-26 21:33:20 -0500

    serving up files

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
-const http = require('http');
+import { createServer } from 'http';
+import { resolve, extname } from 'path';
+import { access, createReadStream } from 'fs';
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-	console.log(req.headers);
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	res.end('<html><body><h1>Hello World!</h1></body></html>');
+const server = createServer((req, res) => {
+	console.log(`Request for ${req.url} by method ${req.method}`);
+
+	if (req.method === 'GET') {
+		let fileUrl = req.url;
+		if (fileUrl === '/') {
+			fileUrl = '/index.html';
+		}
+
+		const filePath = resolve('./public' + fileUrl);
+		const fileExt = extname(filePath);
+
+		if (fileExt === '.html') {
+			access(filePath, (err) => {
+				if (err) {
+					res.statusCode = 404;
+					res.setHeader('Content-Type', 'text/html');
+					res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'text/html');
+
+				createReadStream(filePath).pipe(res);
+			});
+		} else {
+			res.statusCode = 404;
+			res.setHeader('Content-Type', 'text/html');
+			res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
+		}
+	} else {
+		res.statusCode = 404;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
+	}
 });
 
 server.listen(port, hostname, () => {
